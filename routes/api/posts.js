2e35a36e5d48edbde396057dbe91afd3e45bc3b9
sm2101 <sidarthmittal5@gmail.com>
2020-12-07 12:00:55 +0000

    baackend done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -81,5 +81,93 @@ router.delete('/:id',
 		})
 	})
 })
-
+router.post('/like/:id',
+		   passport.authenticate
+			('jwt',
+			 {
+				session:false
+			}),
+		   (req,res) =>{
+	Profile.findOne({user:req.user.id}).then(profile =>{
+		Post.findById(req.params.id).then(post =>{
+			if(post.likes.filter(like => like.user.toString() === req.user.id).length >0){
+				return res.status(400).json({alreadyLiked :'User already liked the post'})
+			}
+			post.likes.unshift({user:req.user.id});
+			
+			post.save().then(post =>{
+				return res.json(post);
+			})
+		}).catch(err =>{
+			res.status(400).json({postnotfound:'No Posts Found'});
+		})
+	})
+})
+router.post('/unlike/:id',
+		   passport.authenticate
+			('jwt',
+			 {
+				session:false
+			}),
+		   (req,res) =>{
+	Profile.findOne({user:req.user.id}).then(profile =>{
+		Post.findById(req.params.id).then(post =>{
+			if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+				return res.status(400).json({notLiked :'You havent liked the post'})
+			}
+			
+			const rIdx = post.likes.map(item => item.user.toString()).indexOf(req.user.id);
+			post.likes.splice(rIdx,1);
+			post.save().then(post =>{
+				res.json(post)
+			})
+		}).catch(err =>{
+			res.status(400).json({postnotfound:'No Posts Found'});
+		})
+	})
+})
+router.post('/comment/:id',
+		   passport.authenticate
+			('jwt',
+			 {
+				session:false
+			}),
+		   (req,res) =>{
+	const {errors, isValid} = validatePostInput(req.body);
+		
+		if(!isValid){
+			return res.status(400).json(errors);
+		}
+	Post.findById(req.params.id).then(post =>{
+		const newComment = {
+			text :req.body.text,
+			name:req.body.name,
+			avatar:req.body.name,
+			user:req.user.id
+		}
+		
+		post.comment.unshift(newComment);
+		post.save()
+			.then(post => res.json(post))
+			.catch(err => res.status(404).json({postNotFound:'No Post Found'}));
+	})
+})
+router.delete('/comment/:id/:comment_id',
+		   passport.authenticate
+			('jwt',
+			 {
+				session:false
+			}),
+		   (req,res) =>{
+	Post.findById(req.params.id).then(post =>{
+		if(post.comment.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
+			return res.status(404).json({noComment:"Comment Doesn't Exist"});
+		}
+		const rIdx = post.comments
+			.map(item => item._id.toString())
+			.indexOf(req.params.comment_id);
+		post.comments.splice(rIdx,1);
+		post.save().then(post => res.json(post));
+	})
+})
 module.exports = router;
